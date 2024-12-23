cd22538f22202cca3d6499736c07dcd30fb84f74
Alois <palois89@protonmail.com>
2024-12-23 17:43:53 +0100

    Feat: Weather result in progress

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,2 +1,37 @@
 import React, {FC} from "react";
+import {WeatherData} from "../store/types";
+
+interface WeatherProps {
+    data: WeatherData;
+}
+
+const Weather: FC<WeatherProps> = ({ data }) => {
+    const farenheit = (data.main.temp * 1.8 - 459.67).toFixed(2);
+    const celsius = (data.main.temp - 273.15).toFixed(2);
+    return (
+    <section>
+        <h1>{data.name} - {data.sys.country}</h1>
+        <ul className="level">
+            <li className="level-item">
+                <p>{data.weather[0].description}</p>
+                <p>
+                    <img src={`https://api.openweathermap.org/img/wm/${data.weather[0].icon}.png`} alt=""/>
+                </p>
+            </li>
+            <li className="level-item">
+                <p>Température</p>
+                <p>{data.main.temp}</p>
+                <p>{celsius} <sup>&#8451;</sup></p>
+                <p>{farenheit} <sup>&#8457;</sup></p>
+            </li>
+            <li className="level-item">
+                <p>Humidité</p>
+                <p>{data.main.humidity}</p>
+            </li>
+        </ul>
+    </section>
+    );
+};
+
+export default Weather;
 
